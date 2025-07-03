c3e9c07500c69dcbbbe02eceae2e14be95f17648
izzimohd <72590832+izzimohd@users.noreply.github.com>
2025-07-03 20:35:43 +0800

    Create 22011232_izzi_L2_medium.cpp

---
+++ 22011232_izzi_L2_medium.cpp
@@ -0,0 +1,25 @@
+/*
+ID=22011232
+Name: Izzi Rafiqie Bin Mohd Ali Hanafiah
+*/
+#include <iostream>
+using namespace std;
+
+int numberOfPaths(int m, int n) {
+
+    if (m == 1 || n == 1)
+        return 1;
+
+
+    //sum the paths coming from the cell above (m-1, n)
+    //and the cell to the left (m, n-1)
+    return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1);
+}
+
+int main() {
+    int m = 3;
+    int n = 3;
+    int res = numberOfPaths(m, n);
+    cout << res << endl;
+    return 0;
+}
